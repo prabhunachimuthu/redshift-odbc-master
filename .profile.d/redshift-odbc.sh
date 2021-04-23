3fefb7e1d975febefcd234abf7d44fe996864a69
prabhu <heroku@softtrends.com>
2021-04-23 10:59:02 +0530

    changes

--- .profile.d/redshift-odbc.sh
+++ .profile.d/redshift-odbc.sh
@@ -1,16 +1,32 @@
 #!/bin/bash
 
-export ODBCSYSINI=${HOME}/.apt/usr/lib/redshift/odbc/conf/
+# export ODBCSYSINI=${HOME}/.apt/usr/lib/redshift/odbc/conf/
 
-echo "[redshift]
-Description=RedshiftDB
-Driver=RedshiftDSIIDriver
-Locale=en-US
-PORT=5439
-SSL=on
-CLIENT_SESSION_KEEP_ALIVE=true
-uid=${REDSHIFT_USERNAME}
-pwd=${REDSHIFT_PASSWORD}
-server=${REDSHIFT_SERVER}
-database=${REDSHIFT_DATABASE}
-" > ${ODBCSYSINI}/odbc.ini
+# echo "[redshift]
+# Description=RedshiftDB
+# Driver=RedshiftDSIIDriver
+# Locale=en-US
+# PORT=5439
+# SSL=on
+# CLIENT_SESSION_KEEP_ALIVE=true
+# uid=${REDSHIFT_USERNAME}
+# pwd=${REDSHIFT_PASSWORD}
+# server=${REDSHIFT_SERVER}
+# database=${REDSHIFT_DATABASE}
+# " > ${ODBCSYSINI}/odbc.ini
+
+# Install the Redshift ODBC driver manager
+apt-get update \
+    && apt-get install -y --no-install-recommends unixodbc
+
+if ! curl -s https://s3.amazonaws.com/redshift-downloads/drivers/odbc/1.4.27.1000/AmazonRedshiftODBC-64-bit-1.4.27.1000-1.x86_64.deb -o driver.deb; then
+    echo 'Failed to download Redshift ODBC Driver!' 1>&2
+    exit 1
+fi
+
+# Install the Redshift ODBC driver
+apt install ./driver.deb
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export ODBCINI=/etc/odbc.ini
+export AMAZONREDSHIFTODBCINI=/etc/amazon.redshiftodbc.ini 
+export ODBCSYSINI=/usr/local/odbc 
